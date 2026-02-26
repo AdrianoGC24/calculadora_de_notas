b695a0799a720be37ed8de1620170ec7e83adf7f
AdrianoGC24 <adrianogonzalezcera002@gmail.com>
2026-02-26 08:01:41 -0500

    primer codigo

--- src/main.py
+++ src/main.py
@@ -2,7 +2,8 @@ print("############################")
 print("### CALCULADORA DE NOTAS ###")
 print("############################")
 
-cantidad_notas = int(input("ingrese la cantidad de notas que fueron tomadas en tu proceso de aprendizaje: "))
+while True:
+    cantidad_notas = int(input("ingrese la cantidad de notas que fueron tomadas en tu proceso de aprendizaje: "))
  
-while true:
-    
+    for i in range(cantidad_notas):
+        notas = int(input("ingrese una nota: "))

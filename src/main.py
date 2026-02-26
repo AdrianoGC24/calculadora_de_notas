011fd534ac2c7ed879dcb218ad568e25e6347dfd
AdrianoGC24 <adrianogonzalezcera002@gmail.com>
2026-02-26 07:52:57 -0500

    primer commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+print("############################")
+print("### CALCULADORA DE NOTAS ###")
+print("############################")
+
+cantidad_notas = int(input("ingrese la cantidad de notas que fueron tomadas en tu proceso de aprendizaje: "))
+ 
+while true:
+    
